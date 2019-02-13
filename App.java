0720027676fa988b5968af3974f33b6e0866c5d6
ameerfrancis <47172044+ameerfrancis@users.noreply.github.com>
2019-02-13 10:44:48 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package ac.za.cput.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
